7d4d5dc16f4181b80ec7fae1a2135e0374959718
leonardorossi <08.leo.rossi@gmail.com>
2024-08-21 01:17:44 -0300

    feat: setup root layout with sidebar and global styles
    - Added `SidebarMenuComponent` for the main layout structure.
    - Integrated Inter font for consistent typography.
    - Configured global styles with base class and minimum height.
    - Set up the main layout structure using flexbox.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import {ReactNode} from "react";
+import SidebarMenuComponent from '@/components/sidebar-menu/sidebar-menu.component';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ReactNode } from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Librarify"
+  title: 'Librarify'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex min-h-dvh bg-slate-950`}>
+        <SidebarMenuComponent />
+        {children}
+      </body>
     </html>
   );
 }
